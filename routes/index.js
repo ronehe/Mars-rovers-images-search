15d5428531b7eeadfe01d26f1bc2230fbe727e40
Leon Markovich <leonmarkovich97@gmail.com>
2021-12-18 18:32:09 +0200

    created 2 register pages with routs to them.

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,10 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('register');
-});
 
+});
+router.post('/registerPassword', function(req, res, next) {
+  res.render('registerPassword');
+
+});
 module.exports = router;
