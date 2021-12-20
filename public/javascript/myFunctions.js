69b3d3db5bc985bf934d493f818668920ce33d30
Leon Markovich <leonmarkovich97@gmail.com>
2021-12-20 11:24:34 +0200

    started working on api

---
+++ public/javascript/myFunctions.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded",()=>{
+
+    document.querySelector("form").addEventListener("submit", function (e) {
+    e.preventDefault();
+
+
+        let theurl = "/api/resources";
+
+        fetch(`${theurl}/${this.querySelector('[type=email]')}`).then(response=>{
+if(response.status>=200&&response.status<300)
+    return response.json()
+else throw response.status.toString() + ' ' + response.statusText + ' ' + response.url
+        }).then(emailNotFound => {
+         if(emailNotFound)
+
+        }).catch();
+
+    })
+    }
+)
