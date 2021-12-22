4e99abe2755085672c2b390a133d56741e47cbb0
Leon Markovich <leonmarkovich97@gmail.com>
2021-12-22 12:31:06 +0200

    added error msg for email in use

--- public/javascript/myFunctions.js
+++ public/javascript/myFunctions.js
@@ -1,24 +1,26 @@
-
 document.addEventListener("DOMContentLoaded", () => {
 
     document.querySelector("form").addEventListener("submit", function (e) {
         e.preventDefault();
-
+        let errMsg=document.getElementById("emailErrorMsg");
+        errMsg.removeChild(errMsg.firstChild)
 
         let theurl = "/api/resources";
 
         fetch(`${theurl}/${this.querySelector('[type=email]').value}`)
             .then((res) => res.json())
             .then((data) => {
-                if(!data.x) {
+                if (!data.x) {
                     document.querySelector('form').submit();
-                }
-                else{
-                    console.log("email already exists")
-
+                } else {
+                    let findError = `<div class="card p-3 alert-warning "><h5 class="text-secondary"><b>
+                                     <strong>Oh No !</strong> the email is already used :( </b></h5></div>`
+                    document.getElementById("emailErrorMsg").innerHTML += findError
                 }
             })
-        .catch((e)=>{console.log("error", e)});
+            .catch((e) => {
+                console.log("error", e)
+            });
 
     })
 })
