c989354e9244ca5d7147ad7ce10603d3f73c0153
Leon Markovich <leonmarkovich97@gmail.com>
2021-12-22 15:03:22 +0200

    still fixing validation

--- public/javascript/myFunctions.js
+++ public/javascript/myFunctions.js
@@ -3,10 +3,16 @@ document.addEventListener("DOMContentLoaded", () => {
     document.querySelector("form").addEventListener("submit", function (e) {
         e.preventDefault();
         let errMsg=document.getElementById("emailErrorMsg");
+        if(errMsg.firstChild)
         errMsg.removeChild(errMsg.firstChild)
 
         let theurl = "/api/resources";
+        let nameElem = this.querySelectorAll('input')[0]
+        let lastNameElem = this.querySelectorAll('input')[1]
+        let emailElem = this.querySelectorAll('input')[2]
 
+
+        if (validateForm(nameElem, lastNameElem,emailElem))
         fetch(`${theurl}/${this.querySelector('[type=email]').value}`)
             .then((res) => res.json())
             .then((data) => {
@@ -24,3 +30,53 @@ document.addEventListener("DOMContentLoaded", () => {
 
     })
 })
+// a module for all string validation functions
+const validatorModule = (() => {
+
+
+    let onlyAlphabet = (name) => {
+        return !/[^a-zA-Z]/.test(name)
+    }
+    let notEmpty=(input)=>{
+        return (!input)
+    }
+
+    let validateName =(name) => {
+        let v =  onlyAlphabet(name)&&notEmpty(name);
+
+        return {
+            isValid: v,
+            message: 'The input must consist only letters'
+
+        }
+    }
+    let validateEmpty =(input) => {
+        let v =   notEmpty(name);
+        return {
+            isValid: v,
+            message: 'The input cant be empty'
+
+        }
+    }
+
+    return {
+        isValidName: validateName,
+        isEmpty:validateEmpty
+    }
+})();
+const validateForm = (name,lastName,email) => {
+
+    let  v1 = validateInput(name, validatorModule.isValidName)
+    let v2 = validateInput(lastName, validatorModule.isValidName)
+    let v3 = validateInput(email, validatorModule.isEmpty())
+
+    return v1 && v2&&v3 ;
+}
+
+const validateInput = (inputElement, validateFunc) => {
+    let errorElement = inputElement.nextElementSibling; // the error message div
+    let v = validateFunc(inputElement.value); // call the validation function
+    errorElement.innerHTML = v.isValid ? '' : v.message; // display the error message
+    v.isValid ? inputElement.classList.remove("is-invalid") : inputElement.classList.add("is-invalid");
+    return v.isValid;
+}
